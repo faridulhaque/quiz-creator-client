5de07cf0555ba6754b413e3e7cd9d11dc32adfec
faridulhaque <faridulhaquemurshed@gmail.com>
2023-03-08 00:45:41 +0600

    small changes

--- src/components/homePageComps/HomeMid.tsx
+++ src/components/homePageComps/HomeMid.tsx
@@ -8,7 +8,7 @@ const HomeMid = () => {
       <div className="w-full h-20 flex items-center justify-between">
         <p className="ml-3 text-xl text-[#539165]">Quiz by you</p>
         <button className="btn align-right bg-[#539165] rounded-md shadow-sm py-3 px-5 mr-3 text-white hover:shadow-gray-700 hover:shadow-sm">
-          Add a quiz
+          Add a question paper
         </button>
       </div>
 
